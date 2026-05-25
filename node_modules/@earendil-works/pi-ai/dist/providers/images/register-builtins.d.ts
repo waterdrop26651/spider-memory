0797ad703c7622eb1a0cd56ae4b09fd6e2ab4761
waterdrop26651 <2268988930@qq.com>
2026-05-25 17:35:41 +0800

    chore: add .gitignore, remove node_modules from tracking

--- node_modules/@earendil-works/pi-ai/dist/providers/images/register-builtins.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ImagesFunction, ImagesOptions } from "../../types.js";
-export declare const generateImagesOpenRouter: ImagesFunction<"openrouter-images", ImagesOptions>;
-export declare function registerBuiltInImagesApiProviders(): void;
-//# sourceMappingURL=register-builtins.d.ts.map
